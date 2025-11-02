14c723b6ee0fdf9f8a7d332d64061222986274b8
MB DEV <mbdev.web13@gmail.com>
2025-11-02 14:26:35 +0100

    build

--- public_web/assets/Dashboard-CNrTRxH4.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-DzDZ6tjL.js";function t(){return e.jsxs("section",{className:"max-w-5xl mx-auto px-4 py-12",children:[e.jsx("h1",{className:"text-2xl font-bold mb-4",children:"Mon espace"}),e.jsx("p",{className:"text-slate-600",children:"À implémenter…"})]})}export{t as default};
-//# sourceMappingURL=Dashboard-CNrTRxH4.js.map
---
+++ public_web/assets/Dashboard-CsI807EC.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-DOxqnmET.js";function t(){return e.jsxs("section",{className:"max-w-5xl mx-auto px-4 py-12",children:[e.jsx("h1",{className:"text-2xl font-bold mb-4",children:"Mon espace"}),e.jsx("p",{className:"text-slate-600",children:"À implémenter…"})]})}export{t as default};
+//# sourceMappingURL=Dashboard-CsI807EC.js.map
